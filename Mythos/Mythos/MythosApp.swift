b682c9338d5fe0f2f53e5a90fe0bcb806d22f212
NarelyLima <narely148@gmail.com>
2023-09-04 10:24:03 -0300

    initial files

---
+++ Mythos/Mythos/MythosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MythosApp.swift
+//  Mythos
+//
+//  Created by Narely Lima on 04/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MythosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
