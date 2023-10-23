6eee6d5f46418e67674be5305d12ef9b348de91b
Cicero Nascimento <antoniociceero@gmail.com>
2023-10-23 20:11:22 -0300

    - animation cemetery card - fix animations hand cards

--- Mythos/Mythos/View/MaybeGameView.swift
+++ Mythos/Mythos/View/MaybeGameView.swift
@@ -34,60 +34,70 @@ struct MaybeGameView: View {
             Image("campo")
                 .resizable()
                 .ignoresSafeArea()
-            VStack{
+            VStack {
                 Spacer()
                 //                    ProgressView("Sua Vida:", value: Double(websocket.myPlayerReference.life), total: 30)
                 //                        .progressViewStyle(GaugeProgressStyle())
                 //                        .frame(width: 90, height: 90)
-
-
-                ZStack {
-                    Image("deck_comprar")
-                        .offset(x: -250, y: 10)
-                    HStack(spacing: -50) {
-                        Spacer()
-                        ForEach(Array(websocket.myPlayerReference.handCards.enumerated()), id: \.element.uuid) {
-                            (index , card) in
-                            CardRepresentable(
-                                isYourTurn: websocket.myPlayerReference.isYourTurn,
-                                isReaction: websocket.myPlayerReference.isReaction,
-                                card: card) {
-                                    self.cardSelected = card
-                                    withAnimation {
-                                        self.isTapped = true
-                                    }
-                                }
-                                .frame(maxHeight: 150)
-                                .scaledToFit()
-                                .offset(y: (index == 0 || index == 2) ? 0 : -15)
-                                .offset(y: cardSelected == card && isTapped ? -UIScreen.main.bounds.height/4 : 0)
-                                .rotationEffect(Angle(degrees: index == 0 ? -5 : (index == 2 ? 5 : 0)))
-                                .zIndex(index == 2 ? 1 : 0) // Coloca a carta do meio na frente
-                        }
-                        .transition(.move(edge: .top))
-                        Spacer()
-                    }
-                    .animation(.easeInOut, value: websocket.myPlayerReference.handCards.count)
-                    .offset(y: 90)
-                    .ignoresSafeArea()
-                    if websocket.myPlayerReference.isYourTurn {
-                        Button {
-                            websocket.sendCard(with: cardSelected)
-                            self.isTapped = false
-                        } label: {
-                            Image("button_descarte")
-                                .frame(maxWidth: 40, maxHeight: 40)
-                                .scaledToFit()
-                        }
-                        .offset(x: 250, y: 10)
-                        .disabled(!isTapped)
-                    }
-                }
-            }
-            .overlay{
                 ForEach(Array(websocket.connectedPlayers.enumerated()), id: \.element.id) { index, player in
                     if websocket.myPlayerReference == player {
                             generatePlayerLayout(for: index, players: websocket.connectedPlayers)
+                    }
+                }
+
+            }
+            .overlay {
+                VStack {
+                    ZStack {
+                        Image("deck_comprar")
+                            .offset(x: -250, y: 110)
+                        HStack(spacing: -50) {
+                            Spacer()
+                            ForEach(Array(websocket.myPlayerReference.handCards.enumerated()), id: \.element.uuid) {
+                                (index , card) in
+                                CardRepresentable(
+                                    isYourTurn: websocket.myPlayerReference.isYourTurn,
+                                    isReaction: websocket.myPlayerReference.isReaction,
+                                    card: card) {
+                                        if self.isTapped {
+                                            withAnimation {
+                                                self.isTapped.toggle()
+                                                self.killTapped = false
+                                            }
+                                        }
+                                        self.cardSelected = card
+                                        withAnimation {
+                                            self.isTapped.toggle()
+                                            self.killTapped = false
+                                        }
+                                    }
+                                    .frame(maxHeight: 150)
+                                    .scaledToFit()
+                                    .offset(y: (index == 0 || index == 2) ? 0 : -15)
+                                    .offset(y: cardSelected == card && isTapped ? -80 : 0)
+                                    .rotationEffect(Angle(degrees: index == 0 ? -5 : (index == 2 ? 5 : 0)))
+                                    .zIndex(index == 2 ? 1 : 0) // Coloca a carta do meio na frente
+                                
+                            }
+                            .transition(.move(edge: .top))
+                            Spacer()
+                        }
+                        .animation(.easeInOut, value: websocket.myPlayerReference.handCards.count)
+                        .offset(y: 200)
+                        .ignoresSafeArea()
+                        if websocket.myPlayerReference.isYourTurn {
+                            Button {
+                                websocket.sendCard(with: cardSelected)
+                                self.isTapped = false
+                                self.killTapped = false
+                            } label: {
+                                Image("button_descarte")
+                                    .frame(maxWidth: 40, maxHeight: 40)
+                                    .scaledToFit()
+                            }
+                            .offset(x: 250, y: 110)
+                            .disabled(!isTapped)
+                        }
                     }
                 }
             }
@@ -130,8 +140,25 @@ struct MaybeGameView: View {
             }
 
         }
-        .navigationBarBackButtonHidden(true)
+        .overlay {
+            if killTapped == true {
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(.black)
+                        .opacity(0.5)
+//                        .fill()
+                        .ignoresSafeArea()
 
+                    KillDeckView(card: websocket.cardsPlayed.last!, killDecktapped: $killTapped)
+                        .scaleEffect(0.5)
+                        .offset(x: 110, y:0)
+                }
+            }
+            if isTapped == true {
+                
+            }
+        }
+        .navigationBarBackButtonHidden(true)
     }
     
     func generatePlayerLayout(for index: Int, players: [PlayerClient]) -> some View {
@@ -170,11 +197,13 @@ struct MaybeGameView: View {
                         Button {
                             withAnimation {
                                 killTapped.toggle()
+                                isTapped = false
                             }
                         } label: {
                             KillDeckView(card: websocket.cardsPlayed.last!, killDecktapped: $killTapped)
                                 .scaleEffect(killTapped ? 1.2 : 0.9)
-                                .offset(x: 0, y: 0)
+                                .offset(x: killTapped ? 110 : 0, y:0)
+                                .opacity(killTapped ? 0 : 1)
                         }
                     }
                 }
